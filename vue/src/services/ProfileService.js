8f150005d4ebd53e7e2042aa6e427866f8ed814e
Rebecca Palmore <rkpalmore@gmail.com>
2022-02-02 11:55:56 -0600

    added profile submit button and updateProfile method

--- vue/src/services/ProfileService.js
+++ vue/src/services/ProfileService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "http://localhost:8080"
 });
 
 export default {
@@ -15,7 +15,7 @@ export default {
     },
 
     editProfile(profile) {
-        return http.put('/editProfile', profile)
+        return http.put('/editProfile', profile);
     },
 
     deleteProfile(username) {
