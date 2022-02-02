ecc328e1cc9b7716cc3aa0bfd598ebfeaa9cb03f
Rebecca Palmore <rkpalmore@gmail.com>
2022-02-02 17:00:41 -0600

    first draft of header

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Header />
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
@@ -7,3 +8,18 @@
     <router-view />
   </div>
 </template>
+
+<script>
+import Header from './components/Header.vue';
+export default {
+  components: { Header },
+}
+</script>
+
+<style>
+#app {
+  height: 100vh;
+  background-color: #FDFFFC;
+}
+
+</style>
---
+++ vue/src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="main">
+    <div id="logo-img">
+      <img src="..\assets\placeholderLogo.png" alt="Placeholder logo" />
+    </div>
+    <div id="logo-text">
+      <span>{{ logo }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header",
+  data() {
+    return {
+      logo: "BIRD NERDS",
+    };
+  },
+};
+</script>
+
+
+<style>
+#main {
+  display: flex;
+  background-color: #011627;
+  height: 150px;
+}
+#logo-img {
+  display: flex;
+  width: 20%;
+  justify-content: center;
+  background-color: #e71d36;
+  border: 2px solid #011627;
+}
+#logo-img > img {
+  padding: 10px;
+}
+#logo-text {
+  display: flex;
+  flex-grow: 1;
+  border-left: 8px solid #fdfffc;
+  color: #fdfffc;
+  align-items: center;
+  font-family: "Bitter", serif;
+}
+span {
+  margin-left: 15%;
+  font-size: 3rem;
+}
+</style>
