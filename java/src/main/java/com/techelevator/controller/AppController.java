3496e669643db690b11e81de6ff86db9b8139412
Amy Pfaff <amycpfaff@gmail.com>
2022-02-03 10:58:08 -0600

    List Controller Methods/Paths

--- java/src/main/java/com/techelevator/controller/AppController.java
+++ java/src/main/java/com/techelevator/controller/AppController.java
@@ -1,6 +1,7 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.ProfileDao;
+import com.techelevator.dao.UserDao;
 import com.techelevator.model.Profile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -15,29 +17,63 @@ import java.security.Principal;
 public class AppController {
 
     @Autowired
-    ProfileDao dao;
+    UserDao userDao;
+
+    @Autowired
+    ProfileDao profileDao;
+
+    @Autowired
+    ListDao listDao;
 
     @RequestMapping(path="/profile", method=RequestMethod.GET)
     public Profile returnProfileByUsername(Principal principal) {
         String username = principal.getName();
-        return dao.getProfile(username);
+        return profileDao.getProfile(username);
     }
 
     @RequestMapping(path="/createProfile", method=RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     public Profile createNewProfile(@RequestBody Profile profile) {
-        return dao.addProfile(profile);
+        return profileDao.addProfile(profile);
     }
 
     @RequestMapping(path="/editProfile", method=RequestMethod.PUT)
     public void editProfile(@RequestBody Profile updatedProfile) {
-        dao.updateProfile(updatedProfile);
+        profileDao.updateProfile(updatedProfile);
     }
 
     @RequestMapping(path="/deleteProfile/{username}", method=RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeProfile(@PathVariable String username) {
-        dao.deleteProfile(username);
+        profileDao.deleteProfile(username);
+    }
+
+    @RequestMapping(path="/lists", method=RequestMethod.GET)
+    public List<BirdList> getAllLists(Principal principal) {
+        String username = principal.getName();
+        int userId = userDao.findIdByUsername(username);
+        return listDao.getLists(userId);
+    }
+
+    @RequestMapping(path="/lists/{listId}", method=RequestMethod.GET)
+    public BirdList getList (@PathVariable int listId) {
+        return listDao.getList(listId);
+    }
+
+    @RequestMapping(path="/createList", method=RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public BirdList createList(@RequestBody BirdList newList) {
+        return listDao.addList(newList);
+    }
+
+    @RequestMapping(path="/editList", method=RequestMethod.PUT)
+    public BirdList editList(@RequestBody BirdList editedList) {
+        return listDao.updateList(editedList);
+    }
+
+    @RequestMapping(path="/deleteList/{listId}", method=RequestMethod.DELETE)
+    public void removeList(@PathVariable int listId) {
+        listDao.deleteList(listId);
     }
 
 }
