4e2828d3ce4541dc33a65442b9cae85e6da66084
Rebecca Palmore <rkpalmore@gmail.com>
2022-02-04 17:24:29 -0600

    minor changes to ListDetail to compile code

--- vue/src/components/ListDetail.vue
+++ vue/src/components/ListDetail.vue
@@ -1,10 +1,15 @@
 <!-- This component is for display a specific list + birds in that list (non-detailed view) -->
 <template>
+
+<div>
+    
+</div>
   
 </template>
 
 <script>
 export default {
+    name: "list-detail",
 
 }
 </script>
