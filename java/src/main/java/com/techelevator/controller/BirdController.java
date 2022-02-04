f6f9defe58d96674dbbd94be1625468608f680dc
Amy Pfaff <amycpfaff@gmail.com>
2022-02-04 13:58:23 -0600

    beginning bird controller

--- java/src/main/java/com/techelevator/controller/BirdController.java
+++ java/src/main/java/com/techelevator/controller/BirdController.java
@@ -1,4 +1,18 @@
 package com.techelevator.controller;
 
+import com.techelevator.dao.BirdDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
 public class BirdController {
+
+    @Autowired
+    BirdDao birdDao;
+
+
 }
